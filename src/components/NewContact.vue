6b5de5f3c23e0e3503a3266a018ee3372811785a
zoume1 <595649828@qq.com>
2019-04-17 10:19:10 +0800

    pull

--- src/components/NewContact.vue
+++ src/components/NewContact.vue
@@ -1,77 +1,86 @@
 <template>
-  <el-row>
-    <el-col :xs="24" :sm="18" :md="14" :lg="10" id="main">
-    <label>姓名：</label>  
-    <el-input v-model="info.name" placeholder="请输入姓名"></el-input>
-    <label>年龄：</label>
-    <el-input v-model="info.age" placeholder="请输入年龄"></el-input>
-    <label>性别：</label>
-    <el-select v-model="info.sex" placeholder="请选择">
-      <el-option v-for="item in options" :key="item" :value="item"></el-option><!-- 这里的key官方推荐在v-for时使用，不然会警告，但不影响使用 -->
-    </el-select>
-    <el-button  class="btn-auto" @click="create" type="success">创建</el-button>
-    <template>
-      <el-table :data="tabledata" align="left">
-        <el-table-column prop="name" label="姓名"></el-table-column>
-        <el-table-column prop="age" label="年龄"></el-table-column>
-        <el-table-column prop="sex" label="性别"></el-table-column>
-        <el-table-column label="操作">
-          <template slot-scope="a">
-            <el-button size="mini" type="danger" @click="del(a.$index)">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </template>
-    </el-col>
-  </el-row>
+  <div style="background-color: #EBEBEB;min-height:900px">
+    <div style="width:100%;background-color: #636363; overflow: hidden">
+            <span class="demonstration" style="float:left;padding-top:10px;color:white;margin-left:1%">
+                网站首页
+            </span>
+      <span class="demonstration" style="float:left;padding:5px;color:white;margin-left:2%;width:15%">
+                <el-input
+                  placeholder="请输入"
+                  icon="search"
+                  v-model="searchCriteria"
+                  :on-icon-click="handleIconClick">
+                </el-input>
+            </span>
+      <span class="demonstration" style="float:right;padding-top:10px;margin-right:1%">
+                <el-dropdown trigger="click">
+                  <span class="el-dropdown-link" style="color:white">
+                    admin<i class="el-icon-caret-bottom el-icon--right"></i>
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>个人信息</el-dropdown-item>
+                    <el-dropdown-item>退出登录</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+            </span>
+    </div>
+
+    <div style="margin-top:5px">
+      <el-row :gutter="10">
+        <el-col :xs="4" :sm="4" :md="4" :lg="4">
+          <div>
+            <el-menu default-active="1" class="el-menu-vertical-demo" style="min-height:900px" @select="handleSelect">
+              <el-menu-item index="1"><i class="el-icon-message"></i>导航一</el-menu-item>
+              <el-menu-item index="2"><i class="el-icon-menu"></i>导航二</el-menu-item>
+            </el-menu>
+          </div>
+        </el-col>
+        <el-col :xs="20" :sm="20" :md="20" :lg="20">
+          <div>
+            <div style="border: 1px solid #A6A6A6; border-radius:6px; padding:5px; margin:2px; background-color: white">
+              <el-breadcrumb separator="/">
+                <el-breadcrumb-item v-for="item in breadcrumbItems" :key="item.id">{{item}}
+
+                </el-breadcrumb-item>
+              </el-breadcrumb>
+            </div>
+          </div>
+
+          <div style="margin-top:10px">
+            <router-view></router-view>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
 </template>
-<script>
-import Storage from '../store/store'
-export default {
-  name: "NewContact",
-  data(){
-    return {
-      info: {
-        name: '',
-        age: null,
-        sex: ''
-      },
-      options: [
-        '女','男','保密'
-      ],
-      tabledata: Storage.fetch()
-    }
-  },
-  methods: {
-    create(){
-      this.tabledata.push(this.info)
-      this.info =  {name: '', age: null, sex: ''}
+<script type="text/ecmascript-6">
+  export default {
+    data(){
+      return {
+        searchCriteria: '',
+        breadcrumbItems: ['导航一'],
+      }
     },
-    del(index){
-      this.tabledata.splice(index,1)
-    }
-  },
-  watch:{
-    tabledata:{
-      handler(items){Storage.save(items)},
-      deep: true
-    }
+
+    methods:{
+      handleIconClick(ev) {
+        console.log(ev);
+      },
+
+      handleSelect(key, keyPath){
+        switch(key){
+          case '1':
+            this.$router.push('/Page1');
+            this.breadcrumbItems  = ['导航一']
+            break;
+          case '2':
+            this.$router.push('/Page2')
+            this.breadcrumbItems  = ['导航二']
+            break;
+        }
+      },
+
+    },
   }
-}
 </script>
-<style>
-    #main{
-      float: none;
-      margin: 0 auto;
-  }
-  .el-input{
-    padding-bottom: 5px;
-  }
-  .el-select {
-      display: block;
-  }
-  .btn-auto{
-      width: 100%;
-      margin-top: 12px;
-  }
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import NewContact from '@/components/NewContact'//新添加，之后在下方的component: NewContact才会生效
+import Page1 from '@/components/page1'
+import Page2 from '@/components/page2'
 Vue.use(Router)
 
 export default new Router({
@@ -14,7 +16,19 @@ export default new Router({
     {
       path: '/newcontact',//和router-link to相呼应，导航到/newcontact
       name: 'NewContact',
-      component: NewContact
+      component: NewContact,
+      children:[
+        {
+          path: '/Page1',
+          name: 'Page1',
+          component: Page1
+        },
+        {
+          path: '/Page2',
+          name: 'Page2',
+          component: Page2
+        },
+      ]
     }
   ]
 })
